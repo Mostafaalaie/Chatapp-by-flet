1d1b682eedb9e9b2ddbe0eea66b5710107cb5761
Mostafaalaie <mostafa.alaie.76@gmail.com>
2023-06-24 16:56:00 +0330

    minor changes for UI

--- chat.py
+++ chat.py
@@ -1,20 +1,72 @@
 import flet as ft
 
 class Message():
-    def __init__(self, user: str, text: str, message_type: str):
-        self.user = user
+    def __init__(self, user_name: str, text: str, message_type: str):
+        self.user_name = user_name
         self.text = text
         self.message_type = message_type
 
+class ChatMessage(ft.Row):
+    def __init__(self, message: Message):
+        super().__init__()
+        self.vertical_alignment="start"
+        self.controls=[
+                ft.CircleAvatar(
+                    content=ft.Text(self.get_initials(message.user_name)),
+                    color=ft.colors.WHITE,
+                    bgcolor=self.get_avatar_color(message.user_name),
+                ),
+                ft.Column(
+                    [
+                        ft.Text(message.user_name, weight="bold"),
+                        ft.Text(message.text, selectable=True),
+                    ],
+                    tight=True,
+                    spacing=5,
+                ),
+            ]
+
+    def get_initials(self, user_name: str):
+        return user_name[:1].capitalize()
+
+    def get_avatar_color(self, user_name: str):
+        colors_lookup = [
+            ft.colors.AMBER,
+            ft.colors.BLUE,
+            ft.colors.BROWN,
+            ft.colors.CYAN,
+            ft.colors.GREEN,
+            ft.colors.INDIGO,
+            ft.colors.LIME,
+            ft.colors.ORANGE,
+            ft.colors.PINK,
+            ft.colors.PURPLE,
+            ft.colors.RED,
+            ft.colors.TEAL,
+            ft.colors.YELLOW,
+        ]
+        return colors_lookup[hash(user_name) % len(colors_lookup)]
+
 
 def main(page: ft.Page):
-
+    
+    page.horizontal_alignment = "stretch"
+    page.title = "Flet Chat"
     chat = ft.Column()
     new_message = ft.TextField()
 
+    def join_click(e):
+        if not user_name.value:
+            user_name.error_text = "Name cannot be blank!"
+            user_name.update()
+        else:
+            page.session.set("user_name", user_name.value)
+            page.dialog.open = False
+            page.pubsub.send_all(Message(user_name=user_name.value, text=f"{user_name.value} has joined the chat.", message_type="login_message"))
+            page.update()
     def on_message(message: Message):
         if message.message_type == "chat_message":
-            chat.controls.append(ft.Text(f"{message.user}: {message.text}"))
+            chat.controls.append(ft.Text(f"{message.user_name}: {message.text}"))
         elif message.message_type == "login_message":
             chat.controls.append(
                 ft.Text(message.text, italic=True, color=ft.colors.BLACK45, size=12)
@@ -24,21 +76,12 @@ def main(page: ft.Page):
     page.pubsub.subscribe(on_message)
 
     def send_click(e):
-        page.pubsub.send_all(Message(user=page.session.get('user_name'), text=new_message.value, message_type="chat_message"))
+        page.pubsub.send_all(Message(user_name=page.session.get('user_name'), text=new_message.value, message_type="chat_message"))
         new_message.value = ""
         page.update()
 
     user_name = ft.TextField(label="Enter your name")
 
-    def join_click(e):
-        if not user_name.value:
-            user_name.error_text = "Name cannot be blank!"
-            user_name.update()
-        else:
-            page.session.set("user_name", user_name.value)
-            page.dialog.open = False
-            page.pubsub.send_all(Message(user=user_name.value, text=f"{user_name.value} has joined the chat.", message_type="login_message"))
-            page.update()
 
     page.dialog = ft.AlertDialog(
         open=True,
